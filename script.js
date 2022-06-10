eb26902682ee83f74f6df83753d87b61bf2825b7
藤田 幸法 <straight.0429@gmail.com>
2022-06-11 01:26:35 +0900

    とりあえずヘッダーとプラグインのところ作ってみました！ 画像は一旦これにして後で変更します！ 勝手に変えていただいて大丈夫です👍

---
+++ script.js
@@ -0,0 +1,8 @@
+$(function () {
+    $(".slide-items").slick({
+        autoplay: true, // 自動再生
+        fade: true, // スライドをフェードイン・フェードアウト
+        cssEase: 'linear',// アニメーション
+        speed: 200, // フェードアニメーションの速度設定
+    });
+})
